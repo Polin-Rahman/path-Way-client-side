d30cde84032894876a8cfb40bc8c0f12b867ddaf
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2021-10-30 02:37:41 +0600

    add order to DB

--- src/components/OrderPlace/OrderPlace.js
+++ src/components/OrderPlace/OrderPlace.js
@@ -1,10 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { useForm } from "react-hook-form";
+import './OrderPlace.css';
+import useAuth from '../../hooks/useAuth';
 
 const OrderPlace = () => {
     const { id } = useParams();
+    const { user } = useAuth();
 
     const [service, setService] = useState({});
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        fetch('http://localhost:5000/orders', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.insertedId) {
+                    alert('Order processed Successfully');
+                    reset();
+                }
+            })
+    };
 
     useEffect(() => {
         const url = `http://localhost:5000/orderplace/${id}`;
@@ -12,22 +34,44 @@ const OrderPlace = () => {
             .then(res => res.json())
             .then(data => setService(data));
     }, [])
-    return (
-        <div className="container">
-            <h2 className="text-center my-5">Confirm Your Booking for - {service.name}</h2>
 
-            <div>
-                <div className="card mb-3" style={{ 'max-width': '900px' }}>
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <img src={service.img} className="img-fluid rounded-start" alt="..." />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                                <p className="card-text">{service.details}</p>
+    return (
+        <div className="container order">
+            <h2 className="text-center my-3">Confirm Your Booking for - {service.name}</h2>
+            <div className="d-flex justify-content-center">
+                <div>
+                    <div className="card mb-3" style={{ 'max-width': '900px' }}>
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <img src={service.img} className="img-fluid rounded-start" alt="..." />
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <p className="card-text">{service.details}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                </div>
+
+            </div>
+            <div className="d-flex justify-content-center">
+                <div>
+                    <form className="order-form" onSubmit={handleSubmit(onSubmit)}>
+                        <p className="mb-1"><small>Booking id:</small></p>
+                        <input defaultValue={id} {...register("orderId")} />
+
+                        <p className="mb-1"><small>Your informations:</small></p>
+                        <input defaultValue={user.displayName} {...register("name")} />
+                        <input defaultValue={user.email} {...register("email", { required: true })} />
+                        {errors.email && <span className="error">This field is required</span>}
+
+                        <input placeholder="Address" defaultValue="" {...register("address", { required: true })} />
+
+                        <input placeholder="Phone number" defaultValue="" {...register("phone", { required: true })} />
+
+                        <input type="submit" />
+                    </form>
                 </div>
             </div>
 
