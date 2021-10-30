755d6af6c901ff71652785c68839bbe44795083c
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2021-10-30 19:01:06 +0600

    delete order done

--- src/components/ManageAllOrders/ManageAllOrders.js
+++ src/components/ManageAllOrders/ManageAllOrders.js
@@ -10,6 +10,25 @@ const ManageAllOrders = () => {
             .then(data => setOrders(data))
     }, [])
 
+    // delete an Order
+    const handleDeleteUser = id => {
+        const proceed = window.confirm('Are you sure, you want to cancel?');
+        if (proceed) {
+            const url = `http://localhost:5000/allorders/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully');
+                        const remainingUsers = orders.filter(user => user._id !== id);
+                        setOrders(remainingUsers);
+                    }
+                });
+        }
+    }
+
     return (
         <div className="container">
             <h3 className="text-center my-5">Manage All Orders</h3>
@@ -26,7 +45,9 @@ const ManageAllOrders = () => {
                             <p className="card-text">Phone: {order.phone}</p>
                             <p className="card-text fw-bolder">Order Status: {order.status} </p>
                             <button className="btn btn-success me-2">Approve Order</button>
-                            <button className="btn btn-danger">Cancel Order</button>
+                            <button
+                                onClick={() => handleDeleteUser(order._id)}
+                                className="btn btn-danger">Cancel Order</button>
                         </div>
                     </div>
                 ))
--- src/components/MyOrders/MyOrders.js
+++ src/components/MyOrders/MyOrders.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 
 const MyOrders = () => {
+
+    const [orders, setOrders] = useState([]);
+    const { user } = useAuth();
+
+    useEffect(() => {
+        fetch('http://localhost:5000/allorders')
+            .then(res => res.json())
+            .then(data => setOrders(data))
+    }, [])
+
+    const userOrders = orders.filter(order => order.email == user.email);
+
+    // delete an Order
+    const handleDeleteUser = id => {
+        const proceed = window.confirm('Are you sure, you want to cancel?');
+        if (proceed) {
+            const url = `http://localhost:5000/allorders/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully');
+                        const remainingUsers = userOrders.filter(user => user._id !== id);
+                        setOrders(remainingUsers);
+                    }
+                });
+        }
+    }
+
     return (
-        <div>
-            <h3>My Orders page</h3>
+        <div className="container">
+            <h3 className="text-center my-5">My Orders</h3>
+
+            {
+                userOrders.map(order => (
+                    <div className="card my-3">
+                        <div className="card-header">
+                            Order Id: {order.orderId}
+                        </div>
+                        <div className="card-body">
+                            <h5 className="card-title">Name: {order.name}</h5>
+                            <p className="card-text">Email: {order.email}</p>
+                            <p className="card-text">Address: {order.address}</p>
+                            <p className="card-text">Phone: {order.phone}</p>
+                            <p className="card-text fw-bolder">Order Status: {order.status} </p>
+                            <button onClick={() => handleDeleteUser(order._id)}
+                                className="btn btn-danger">Cancel Order</button>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     );
 };
